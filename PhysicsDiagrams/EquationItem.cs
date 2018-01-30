5d3197d3505355dc7e33aead7cf9a6813c2c3def
Alan-Bi <alan.bi326@gmail.com>
2018-01-29 21:23:55 -0800

    Code to display equations on main page

---
+++ PhysicsDiagrams/EquationItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UIKit;
+using System.Drawing;
+
+namespace PhysicsDiagrams
+{
+    public class EquationItem : UIView
+    {
+        private UIView itemView;
+
+        private const float width = 300;
+        private const float height = 200;
+
+        public EquationItem(float x, float y, string label, string equation)
+        {
+            itemView = new UIView(new RectangleF(x, y, width, height));
+            itemView.BackgroundColor = UIColor.LightGray;
+
+            var itemLabel = new UILabel(new RectangleF(0, 0, width, height / 5));
+            itemLabel.Text = label;
+            itemView.AddSubview(itemLabel);
+
+            // Diagram image will probably be here between the two
+
+            var itemEquation = new UILabel(new RectangleF(0, height * 4 / 5, width, height / 5));
+            itemEquation.Text = equation;
+            itemView.AddSubview(itemEquation);
+
+            this.AddSubview(itemView);
+        }
+    }
+}
